b46b94d56da72bd9f82c9faaa0fce6d807f718c1
astra <26215963+3tternp@users.noreply.github.com>
2025-06-10 13:58:58 +0545

    Create urls.py

---
+++ backend/lms/urls.py
@@ -0,0 +1,6 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+]
