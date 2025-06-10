fb1ef0dcccc6152abcb75846ac5dd643fe908cc1
astra <26215963+3tternp@users.noreply.github.com>
2025-06-10 14:20:26 +0545

    Create serializers.py

---
+++ backend/users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
