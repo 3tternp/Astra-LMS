66af440394d880fcdc60d9ab9e3bbb22f1237e1d
astra <26215963+3tternp@users.noreply.github.com>
2025-06-10 15:00:44 +0545

    Create ExampleComponent.js

---
+++ frontend/src/components/ExampleComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+  return (
+    <div>
+      <p>This is an example component inside Astra LMS frontend.</p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
